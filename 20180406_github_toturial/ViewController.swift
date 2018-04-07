a95b704db625ef5b0dae542091e6126f766f3ee3
Hsin-Hung Chen <cxh@ms1.url.com.tw>
2018-04-07 18:21:56 +0800

    Initial Commit

---
+++ 20180406_github_toturial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  20180406_github_toturial
+//
+//  Created by chen hsin hung on 2018/4/7.
+//  Copyright © 2018年 chen hsin hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
