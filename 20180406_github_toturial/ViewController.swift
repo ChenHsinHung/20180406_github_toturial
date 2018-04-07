426830bbd0274785b06d9150a1c76a49c4ea718f
Hsin-Hung Chen <cxh@ms1.url.com.tw>
2018-04-07 18:56:43 +0800

    功能一 完成

--- 20180406_github_toturial/ViewController.swift
+++ 20180406_github_toturial/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //追加 功能一
+
     }
 
     override func didReceiveMemoryWarning() {
