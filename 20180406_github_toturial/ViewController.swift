575c28a8f83bf8fed0fa543ae4098a4279a40e4a
Hsin-Hung Chen <cxh@ms1.url.com.tw>
2018-04-07 18:58:06 +0800

    功能二完成

--- 20180406_github_toturial/ViewController.swift
+++ 20180406_github_toturial/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //追加 功能一
+        //功能二 完成
+        
 
     }
 
